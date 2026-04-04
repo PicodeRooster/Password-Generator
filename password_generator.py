6c7dd0849079f744f1c7abf277560e2174e418f8
Coco Moreau <91811230+PicodeRooster@users.noreply.github.com>
2026-04-04 13:07:15 -0400

    Refactored code logic
    Replaced all redundant lines using secrets.randbelow() - represented in number_generator() function - to secrets.choice() to improve redability and reduce lines of code.

--- password_generator.py
+++ password_generator.py
@@ -27,19 +27,17 @@ def scramble_sequence(string_item):
 def generate_sequence():
     password_list = list()
     password_list.append(secrets.choice(alphabet).upper())                                         #Include at least 1 uppercase letter
-    password_list.append(alphabet[number_generator(len(alphabet))].lower())                                         #Include at least 1 lowercase letter
+    password_list.append(secrets.choice(alphabet).lower())                                         #Include at least 1 lowercase letter
     password_list.append(number_generator(10))                                                                      #Include at least 1 single digit integer
-    password_list.append(symbols[number_generator(len(symbols))])                                                   #Include 1 symbol                                                        
+    password_list.append(secrets.choice(symbols)                                                   #Include 1 symbol                                                        
 
     while len(password_list) < 12:
         token = number_generator(2)
         match token:
             case 0:
-                index = number_generator(len(alphabet))
-                password_list.append(alphabet[index])
+                password_list.append(secrets.choice(alphabet))
             case 1:
-                num = number_generator(10)
-                password_list.append(num)
+                password_list.append(number_generator(10))
 
     sequence_one = "".join(map(str, password_list))
     sequence_two = scramble_sequence(sequence_one)
