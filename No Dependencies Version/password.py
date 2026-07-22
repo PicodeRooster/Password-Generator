3799b562e2bc969a8b396e6aa68d3857e2da0923
Coco Moreau <91811230+PicodeRooster@users.noreply.github.com>
2026-07-22 21:06:51 +0000

    Refactor password generator no dependencies version to match code almost to 100% exact as other version

--- No Dependencies Version/password.py
+++ No Dependencies Version/password.py
@@ -1,6 +1,5 @@
 import string
 import secrets
-import re
 
 alphabet = list(string.ascii_letters)
 symbols = ["!", "@", "#", "$", "%", "&", "*", "?", "~", "/"]
@@ -8,45 +7,47 @@ symbols = ["!", "@", "#", "$", "%", "&", "*", "?", "~", "/"]
 def number_generator(x):
     return secrets.randbelow(x)
 
-def find_consecutives(s: str) -> bool:
-    return bool(re.search(r'[A-Za-z]{3,}|\d{3,}', s))
-
-def scramble_sequence(string_item):
-    seq = list(string_item)
-    scrambled_seq = list()
-
-    while len(seq) > 1:
-        index = secrets.randbelow(len(seq))
-        scrambled_seq.append(seq[index])
-        seq.pop(index)
-        
-    scrambled_seq.append("".join(seq))
-    return "".join(scrambled_seq)
+def patch_sequence(a, b):
+    index = number_generator(len(alphabet))
+    c = alphabet[index]
+    seq = f"{b}{a}{c}"
+    return seq
 
 def generate_sequence():
-    password_list = list()
-    password_list.append(secrets.choice(alphabet).upper())                                         #Include at least 1 uppercase letter
-    password_list.append(alphabet[number_generator(len(alphabet))].lower())                                         #Include at least 1 lowercase letter
-    password_list.append(number_generator(10))                                                                      #Include at least 1 single digit integer
-    password_list.append(symbols[number_generator(len(symbols))] for _ in range(2))                                  #Include 2 symbols                                                 #Include 1 symbol                                                        
-                                                                                                               
-    while len(password_list) < 12:
+    password_set = set()
+    password_set.add(alphabet[number_generator(len(alphabet))])
+    password_set.add(number_generator(10))
+    password_set.update(symbols[number_generator(len(symbols))] for _ in range(2))
+
+    while len(password_set) < 12:
         token = number_generator(2)
         match token:
             case 0:
                 index = number_generator(len(alphabet))
-                password_list.append(alphabet[index])
+                password_set.add(alphabet[index])
             case 1:
                 num = number_generator(10)
-                password_list.append(num)
+                password_set.add(num)
 
-    sequence_one = "".join(map(str, password_list))
-    sequence_two = scramble_sequence(sequence_one)
     
-    while find_consecutives(sequence_two):
-        sequence_two = scramble_sequence(sequence_one)
+    sequence = ""
+    nested_sequence = []
+    for char in password_set:
+        sequence += str(char)
 
-    return sequence_two
+        if len(sequence) == 3:
+            nested_sequence.append(sequence)
+            sequence = ""
+    
+    final_sequence = ""
+    for pad in nested_sequence:
+        if (pad == "".join(sorted(pad))):
+            new_pad = patch_sequence(pad[0], pad[1])
+            final_sequence += new_pad
+        else:
+            final_sequence += pad
+    
+    return final_sequence
 
 password = generate_sequence()
 print(f"Generated password: {password}")
